d6408f6c20934d85a42f2170da0170c88390cbb6
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-01-10 14:37:28 +0700

    feat: add POST request

--- src/index.js
+++ src/index.js
@@ -5,21 +5,34 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+// call middleware
+app.use(express.json()); // to allow sending json to the server
+app.use(express.urlencoded()); // parsed to urlencoded, so it allow sendirg urlencoded to the server
+
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
 
+// mock database
+const groceryList = [
+  {
+    item: "milk",
+    quantity: 2,
+  },
+  {
+    item: "cereal",
+    quantity: 1,
+  },
+  {
+    item: "pop-tarts",
+    quantity: 1,
+  },
+];
+
 app.get("/groceries", (req, res) => {
-  res.send([
-    {
-      item: "milk",
-      quantity: 2,
-    },
-    {
-      item: "cereal",
-      quantity: 1,
-    },
-    {
-      item: "pop-tarts",
-      quantity: 1,
-    },
-  ]);
+  res.send(groceryList);
+});
+
+app.post("/groceries", (req, res) => {
+  console.log(req.body);
+  groceryList.push(req.body);
+  res.send(201);
 });
