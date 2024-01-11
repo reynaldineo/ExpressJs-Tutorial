c5f290fca4d0bdf0efdd75a1aeb6e5726d172dba
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-01-11 23:30:35 +0700

    feat: add Query Parameters

--- src/routes/markets.js
+++ src/routes/markets.js
@@ -4,18 +4,39 @@ const router = Router();
 
 const supermarkets = [
   {
+    id: 1,
     store: "Whole Foods",
+    miles: 0.6,
   },
   {
+    id: 2,
     store: "Trader Joes",
+    miles: 2.5,
   },
   {
+    id: 3,
     store: "Albertsons",
+    miles: 2.8,
+  },
+  {
+    id: 4,
+    store: "Trader Joes",
+    miles: 3.5,
+  },
+  {
+    id: 5,
+    store: "Albertsons",
+    miles: 1.8,
   },
 ];
 
 router.get("/", (req, res) => {
-  res.send(supermarkets);
+  const { miles } = req.query;
+  const parsedMiles = parseInt(miles);
+  if (!isNaN(parsedMiles)) {
+    const filteredStore = supermarkets.filter((s) => s.miles <= parsedMiles);
+    res.send(filteredStore);
+  } else res.send(supermarkets);
 });
 
 router.get("/:store", (req, res) => {
